633f090d381a8ae2e1651895e144cc2bcc4a50ea
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-06-03 21:07:19 -0400

    chore(.eslintrc): Add more rules

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -16,34 +16,33 @@ const INITIAL_STATE = fromJS({
 
 function sessionReducer(state = INITIAL_STATE, action = {}) {
   switch (action.type) {
+    case LOGIN_USER_PENDING:
+      return state.merge(fromJS({
+        token: null,
+        user: {},
+        hasError: false,
+        isLoading: true,
+      }));
 
-  case LOGIN_USER_PENDING:
-    return state.merge(fromJS({
-      token: null,
-      user: {},
-      hasError: false,
-      isLoading: true,
-    }));
+    case LOGIN_USER_SUCCESS:
+      return state.merge(fromJS({
+        token: action.payload.token,
+        user: action.payload.profile,
+        hasError: false,
+        isLoading: false,
+      }));
 
-  case LOGIN_USER_SUCCESS:
-    return state.merge(fromJS({
-      token: action.payload.token,
-      user: action.payload.profile,
-      hasError: false,
-      isLoading: false,
-    }));
+    case LOGIN_USER_ERROR:
+      return state.merge(fromJS({
+        hasError: true,
+        isLoading: false,
+      }));
 
-  case LOGIN_USER_ERROR:
-    return state.merge(fromJS({
-      hasError: true,
-      isLoading: false,
-    }));
+    case LOGOUT_USER:
+      return state.merge(INITIAL_STATE);
 
-  case LOGOUT_USER:
-    return state.merge(INITIAL_STATE);
-
-  default:
-    return state;
+    default:
+      return state;
   }
 }
 
