3efa5903c6560067fdf7027eee8834bf76e30a2d
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-06-03 21:20:56 -0400

    refactor(*): Fix linting errors

--- src/components/form/group/index.js
+++ src/components/form/group/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function FormGroup({ children, ...props}) {
+function FormGroup({ children, ...props }) {
   return (
     <div className="py2" {...props}>
       { children }
