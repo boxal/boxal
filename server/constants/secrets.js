88ecc777e6367004346932ff1ae51d1cbd3ec052
Brandon <brandon.bowen@mail.utoronto.ca>
2016-08-08 14:18:59 -0400

    initial commit for the Integrate Socket.io and Webscraper with Boxal

--- server/constants/secrets.js
+++ server/constants/secrets.js
@@ -1,5 +1,5 @@
 export const FACEBOOK = {
-  CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
-  CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
-  CALLBACK_URL: process.env.FACEBOOK_CALLBACK_URL,
+  CLIENT_ID: '481171105425490',
+  CLIENT_SECRET: 'd1a8505a1a54754637671fe194b944bd',
+  CALLBACK_URL: 'http://localhost:8080/api/auth/facebook/callback',
 };
