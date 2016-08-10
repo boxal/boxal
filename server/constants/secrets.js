49e44fef9263dc6e328120c660caacc81b232be9
Brandon <brandon.bowen@mail.utoronto.ca>
2016-08-10 09:30:18 -0400

    removed the file that shall not be spoken about

--- server/constants/secrets.js
+++
@@ -1,5 +0,0 @@
-export const FACEBOOK = {
-  CLIENT_ID: '481171105425490',
-  CLIENT_SECRET: 'd1a8505a1a54754637671fe194b944bd',
-  CALLBACK_URL: 'http://localhost:8080/api/auth/facebook/callback',
-};
